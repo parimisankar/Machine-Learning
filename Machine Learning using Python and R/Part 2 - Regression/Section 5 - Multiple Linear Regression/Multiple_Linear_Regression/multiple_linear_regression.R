1387222089d91f57a8e072098b1a1483bbd464b0
Jaya Shankar Parimi <parimisankar@users.noreply.github.com>
2018-04-15 13:57:11 -0400

    MLR

--- Machine Learning using Python and R/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.R
+++ Machine Learning using Python and R/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.R
@@ -1,28 +1,80 @@
-# Multiple Linear Regression
+# Data Preprocessing
+# 1. Import libraries
+# - Not needed in R
 
-# Importing the dataset
+# 2. Importing the dataset
 dataset = read.csv('50_Startups.csv')
 
-# Encoding categorical data
-dataset$State = factor(dataset$State,
-                       levels = c('New York', 'California', 'Florida'),
-                       labels = c(1, 2, 3))
+# 3. Split data into features/independent variables(X) and dependent(y) variables
+# - Not needed in R
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+# Data Manipulation and Cleaning
+# 4. Taking care of missing data
+dataset$R.D.Spend = ifelse(is.na(dataset$R.D.Spend),
+                                 ave(dataset$R.D.Spend, FUN = function(x) mean(x, na.rm = TRUE)),
+                                 dataset$R.D.Spend)
+dataset$Administration = ifelse(is.na(dataset$Administration),
+                           ave(dataset$Administration, FUN = function(x) mean(x, na.rm = TRUE)),
+                           dataset$Administration)
+dataset$Marketing.Spend = ifelse(is.na(dataset$Marketing.Spend),
+                        ave(dataset$Marketing.Spend, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Marketing.Spend)
+dataset$Profit = ifelse(is.na(dataset$Profit),
+                                 ave(dataset$Profit, FUN = function(x) mean(x, na.rm = TRUE)),
+                                 dataset$Profit)
+
+# # 5. Encoding categorical data
+dataset$State = factor(dataset$State,
+                        levels = c('New York', 'California', 'Florida'),
+                        labels = c(1, 2, 3))
+# dataset$Purchased = factor(dataset$Purchased,
+#                            levels = c('Yes', 'No'),
+#                            labels = c(1, 0))
+
+# # 6. Feature Scaling - helps in rapid conversion of ML Models
+dataset[, 1:3] = scale(dataset[, 1:3])
+dataset[, 5] = scale(dataset[, 5])
+
+# Train and test samples
+# 7. Splitting the dataset into the Training set and Test set
+# install.packages('caTools') - Install package caTools from packages tab or use command
+# library(caTools) - Selects and imports the library
 library(caTools)
-set.seed(123)
-split = sample.split(dataset$Profit, SplitRatio = 0.8)
+set.seed(123) # 123 can be any random number
+split = sample.split(dataset$Profit, SplitRatio = 0.8) # Split using dependent variable (in this case, Salary)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+# Ensure size(Training_set) > size(Test_set)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Multiple Linear Regression to the Training set
+# Model Development - Simple Regression
+# # 8. Fitting Simple Linear Regression to the Training set
+# regressor = lm(formula = Salary ~ YearsExperience,
+#                data = training_set)
+# 8. Fitting Multiple Linear Regression to the Training set
 regressor = lm(formula = Profit ~ .,
                data = training_set)
 
-# Predicting the Test set results
+# 9. Predicting the Test set results
 y_pred = predict(regressor, newdata = test_set)
+
+# 10. Evaluate/Diagnose the Model - Mean Absolute Percentage Error (MAPE)
+# Look at the information in Regressor
+# summary(regressor) 
+# - Look for Signif. codes (more starts --> better)
+# - Look for P value (<0.005 is good)
+# 
+# # 11. Model Visualization
+# # Visualising the Training set results
+# # install.packages('ggplot2') - Install package ggplot2 from packages tab or use command
+# # library(ggplot2) - Selects and imports the library
+# library(ggplot2)
+# ggplot() +
+#   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary),
+#              colour = 'green') +
+#   geom_line(aes(x = training_set$YearsExperience, y = predict(regressor, newdata = training_set)),
+#             colour = 'blue') +
+#   geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary),
+#              colour = 'red') +
+#   ggtitle('Salary vs Experience(Green - Training Set, Red - Test Set)') +
+#   xlab('Years of experience') +
+#   ylab('Salary')
--- Machine Learning using Python and R/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
+++ Machine Learning using Python and R/Part 2 - Regression/Section 5 - Multiple Linear Regression/Multiple_Linear_Regression/multiple_linear_regression.py
@@ -1,41 +1,71 @@
 # Multiple Linear Regression
+# Data Preprocessing
 
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+# 1. Importing the libraries
+import numpy as np # Contains mathematical tools
+import matplotlib.pyplot as plt # Contains plotting tools
+import pandas as pd # Contains dataset import/manage tools
 
-# Importing the dataset
+# 2. Importing the dataset
 dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 4].values
 
-# Encoding categorical data
+# 3. Divide the dataset into independent(X) and dependent(y) variables
+X = dataset.iloc[:, :-1].values # Matrix of features/variables
+y = dataset.iloc[:, 1].values # Matrix of outputs/prediction/result
+
+# Data Manipulation and Cleaning
+# 4. Take care of missing data
+from sklearn.preprocessing import Imputer # Imputer is a class that takes care of missing data
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 0:3])
+X[:, 0:3] = imputer.transform(X[:, 0:3])
+
+# 5. Encode categorical data
+# Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder = LabelEncoder()
-X[:, 3] = labelencoder.fit_transform(X[:, 3])
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
 onehotencoder = OneHotEncoder(categorical_features = [3])
 X = onehotencoder.fit_transform(X).toarray()
 
 # Avoiding the Dummy Variable Trap
 X = X[:, 1:]
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+# Encoding the Dependent Variable
+"""labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)"""
+
+# 6. Feature Scaling (Standardisation (StandardScaler) or Normalisation (Normalizer))
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X = sc_X.fit_transform(X)
+sc_y = StandardScaler()
+y = sc_y.fit_transform(y)
+
+# Test and Train Samples
+# 7. Splitting the dataset into the Training set and Test set
+# from sklearn.cross_validation import train_test_split # cross_validation is deprecated and moved to model_selection
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
-
+# Model Development - Simple Regression
+# 8. Fitting Simple Linear Regression to the Training set
 # Fitting Multiple Linear Regression to the Training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
+# 9. Predicting the Test set results
 y_pred = regressor.predict(X_test)
+
+# 10. Evaluate/Diagnose the Model - Mean Absolute Percentage Error (MAPE)
+
+# 11. Model Visualization
+# Visualising the Training set results
+"""plt.scatter(X_train, y_train, color = 'green')
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Green - Training set, Red - Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()"""
